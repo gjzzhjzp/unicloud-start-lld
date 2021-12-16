b6cafbe941b3236192c22bb9a89d51caef6df09f
lanyunshan <872610221@qq.com>
2021-12-16 22:48:19 +0800

    a

---
+++ pages/detail/nineSquared.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="nineSquared">
+		
+		<view class="nineSquared_1" v-if="list.length==1">
+			<view class="nine-img">
+				<small-image :data="list[0]" ref="smallImage1"></small-image>
+			</view>
+		</view>
+		<view class="nineSquared_2" v-else-if="list.length==2||list.length==4">
+			<u-row gutter="16">
+				<u-col span="6"  v-for="(item,index) in list"  :key="index">
+					<small-image :data="item" ref="smallImage2"></small-image>
+				</u-col>
+			</u-row>
+		</view>
+		<view class="nineSquared_3" v-else>
+			<u-row gutter="16">
+				<u-col span="4"  v-for="(item,index) in list"  :key="index">
+					<small-image :data="item" ref="smallImage3"></small-image>
+				</u-col>
+			</u-row>
+		</view>
+	</view>
+</template>
+<script>
+	import smallImage from "./smallImage.vue"
+	export default{
+		name:"nineSquared",
+		data(){
+			return {
+				
+			}
+		},
+		components:{
+			smallImage
+		},
+		props:{
+			list:{
+				type:Array,
+				default(){
+					return []
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+	.nineSquared_3 .nine-img{
+		width: 33.33%;
+		display: inline-block;
+		position: relative;
+	}
+</style>
---
+++ pages/detail/smallImage.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="smallImage">
+		<u-lazy-load  v-if="data.url.indexOf('gif')==-1" @click="previewOpen(data)"
+		 threshold="300" height="100px" border-radius="10" :image="data.url"></u-lazy-load>
+		<image v-else :src="data.url"  @click="previewOpen(data)"  mode="aspectFill"></image>
+	</view>
+</template>
+
+<script>
+	export default{
+		name:"smallImage",
+		data(){
+			return {
+				
+			}
+		},
+		props:{
+			data:{
+				type:Object,
+				default(){
+					return {}
+				}
+			}
+		},
+		methods:{
+			previewOpen(data){
+				this.$emit("preview",data);
+			}
+		}
+	}
+</script>
+
+<style>
+	.smallImage{
+		height: 300rpx;
+		width: 100%;
+	}
+</style>

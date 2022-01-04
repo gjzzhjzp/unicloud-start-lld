440fb05871a400225a5c535453cd1901107bc47f
lanyunshan <872610221@qq.com>
2022-01-04 22:45:25 +0800

    1

--- components/jz-sy-list/jz-sy-list.vue
+++ components/jz-sy-list/jz-sy-list.vue
@@ -1,8 +1,8 @@
 <template>
 	<view>
 		<view class="jz-sy-list-section">
-			<u-section line-color="#7275D3" :font-size="32" :title="title" :right="showright" sub-title="查看更多>>" :arrow="false"
-				@click="$notMoreTap(tomore,'notTap')"></u-section>
+			<u-section line-color="#7275D3" :font-size="32" :title="title" :right="showright" sub-title="查看更多>>"
+				:arrow="false" @click="$notMoreTap(tomore,'notTap')"></u-section>
 		</view>
 		<view class="jz-sy-list">
 			<template v-if="!isEmpty">
@@ -13,7 +13,9 @@
 								<!-- <image-storage :src="imageUrl(item)" height="100px" width="100%" :customStyle="{borderRadius:'6px'}" mode="aspectFill"></image-storage> -->
 								<!-- <u-lazy-load threshold="300" height="100px" border-radius="10" img-mode="aspectFill"
 									:image="imageUrl(item)"></u-lazy-load> -->
-								<u-image width="100%" height="100px" border-radius="10" :src="imageUrl(item)" loading-icon="/static/center/chang1.png" error-icon="/static/center/error_chang.png">
+								<u-image width="100%" height="100px" border-radius="10" :src="imageUrl(item)"
+									loading-icon="/static/center/chang1.png"
+									error-icon="/static/center/error_chang.png">
 								</u-image>
 							</view>
 							<view class="jz-sy-list-text">{{item.title}}</view>
@@ -48,9 +50,9 @@
 			}
 		},
 		props: {
-			rows:{
-				type:Number,
-				default(){
+			rows: {
+				type: Number,
+				default () {
 					return 4
 				}
 			},
@@ -80,21 +82,21 @@
 					return true
 				}
 			},
-			zy_gs:{
+			zy_gs: {
 				type: Number,
 				default () {
 					return undefined
 				}
 			},
-			categories:{
+			categories: {
 				type: String,
 				default () {
 					return ""
 				}
 			},
-			ignore:{
-				type:String,
-				default(){
+			ignore: {
+				type: String,
+				default () {
 					return ""
 				}
 			}
@@ -114,9 +116,15 @@
 			},
 			tomore() {
 				if (this.type) {
-					uni.navigateTo({
-						url: '/pages/resource/list?type=' + this.type
-					});
+					if (this.type == "tj") {
+						uni.navigateTo({
+							url: '/pages/moretj/moretj'
+						});
+					} else {
+						uni.navigateTo({
+							url: '/pages/resource/list?type=' + this.type
+						});
+					}
 				} else {
 					uni.navigateTo({
 						url: '/pages/resource/list?title=' + this.title
@@ -124,7 +132,7 @@
 				}
 			},
 			getList() {
-				var app_bbh="115";
+				var app_bbh = "115";
 				//#ifdef APP-PLUS
 				app_bbh = plus.runtime.versionCode;
 				//#endif
@@ -133,22 +141,22 @@
 					label: this.label,
 					zy_gs: [0, 1, 3],
 					page: 1,
-					rows: this.rows||4,
+					rows: this.rows || 4,
 					app_bbh: app_bbh
 				}
-				if(typeof this.zy_gs!="undefined"){
-					Object.assign(param,{
-						zy_gs:this.zy_gs
+				if (typeof this.zy_gs != "undefined") {
+					Object.assign(param, {
+						zy_gs: this.zy_gs
 					});
 				}
-				if(this.categories){
-					Object.assign(param,{
-						categories:this.categories
+				if (this.categories) {
+					Object.assign(param, {
+						categories: this.categories
 					});
 				}
-				if(this.ignore){
-					Object.assign(param,{
-						ignore:this.ignore
+				if (this.ignore) {
+					Object.assign(param, {
+						ignore: this.ignore
 					});
 				}
 				uniCloud.callFunction({
@@ -163,6 +171,44 @@
 						this.list = res.rows;
 						if (this.list.length > 0) {
 							this.isEmpty = false;
+							if (this.type == "tj") {
+								this.list.unshift({
+									"_id": "61aee1840d111e00012bf162",
+									"article_status": 1,
+									"zy_gs": 1,
+									"publish_date": 1638850948344,
+									"last_modify_date": 1641298665437,
+									"is_grant": 0,
+									"is_encryption": 0,
+									"is_recommend": 0,
+									"author": "俊哲宇宙APP",
+									"title": "投稿教程",
+									"avatar": [{
+										"extname": "jpg",
+										"name": "zyimage1638850857812",
+										"url": "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-3a72b088-8226-498e-a9cc-a695d0ed4ce7/c521d686-d0e7-4dae-8206-d6e80c4448c3.",
+										"path": "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-3a72b088-8226-498e-a9cc-a695d0ed4ce7/c521d686-d0e7-4dae-8206-d6e80c4448c3."
+									}]
+								})
+								this.list.unshift({
+									"_id": "61ca6e97344587000187e9b2",
+									"article_status": 1,
+									"zy_gs": 0,
+									"publish_date": 1640656535652,
+									"last_modify_date": 1641298663223,
+									"is_grant": 1,
+									"is_encryption": 0,
+									"is_recommend": 0,
+									"author": "俊哲宇宙APP",
+									"title": "投稿规则（试用版）",
+									"avatar": [{
+										"extname": "jpg",
+										"name": "zyimage1640656499956",
+										"url": "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-3a72b088-8226-498e-a9cc-a695d0ed4ce7/b80c8e52-08ae-4201-a8f1-fc9bc1f54e0e.",
+										"path": "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-3a72b088-8226-498e-a9cc-a695d0ed4ce7/b80c8e52-08ae-4201-a8f1-fc9bc1f54e0e."
+									}]
+								})
+							}
 						} else {
 							this.isEmpty = true;
 						}
@@ -172,11 +218,11 @@
 							type: 'error'
 						});
 					}
-				}).catch((err)=>{
-					console.log("网络错误，请重试——err",err);
+				}).catch((err) => {
+					console.log("网络错误，请重试——err", err);
 					uni.showModal({
-					  content: err.message || '网络错误，请重试',
-					  showCancel: false
+						content: err.message || '网络错误，请重试',
+						showCancel: false
 					});
 				});
 			},
@@ -184,12 +230,13 @@
 	}
 </script>
 <style lang="scss">
-	.jz-sy-item{
-		    padding: 0px 6px !important;
+	.jz-sy-item {
+		padding: 0px 6px !important;
 	}
+
 	.jz-sy-list-item {
 		margin: 6px 0px;
-		    width: 100%;
+		width: 100%;
 	}
 
 	.jz-sy-list-text {

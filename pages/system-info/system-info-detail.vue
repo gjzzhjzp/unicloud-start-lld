95507f6bddf2af5f788637e601edb571344bfbc4
兰云杉 <872610221@qq.com>
2022-01-18 10:47:37 +0800

    1

--- pages/system-info/system-info-detail.vue
+++ pages/system-info/system-info-detail.vue
@@ -3,7 +3,8 @@
 		<u-navbar :is-back="true" title="系统消息"></u-navbar>
 		<view class="jz-container">
 			<scroll-view id="scroll-Y" :scroll-top="scrollTop" scroll-y="true" class="scroll-Y">
-				<view class="all-system-info" id="all-system-info" :style="{'visibility': showinfo?'':'hidden'}">
+				<!-- :style="{'visibility': showinfo?'':'hidden'}" -->
+				<view class="all-system-info" id="all-system-info">
 					<view class="infos" v-for="(item,index) in infos" :key="index">
 						<yd-chatitem ref="chatitem" :message="item.comment" :leftTime="item.comment_date"></yd-chatitem>
 					</view>
@@ -24,7 +25,7 @@
 			return {
 				scrollTop:0,
 				infos: [],
-				showinfo:false,
+				// showinfo:false,
 				infotype:"",///消息类型
 				message: "你的投稿视频【<span class='zyid' id='619ce4e1808fdd00017261d3'>蜀道难，难于上青天</span>】有宝子【】评论啦~~【啊啊啊啊啊啊啊这里是内容】"
 			}
@@ -34,13 +35,13 @@
 			this.getinfos();
 		},
 		mounted() {
-			var that=this;
-			setTimeout(()=>{
-				// debugger;
-				var _height=$("#all-system-info").height();
-				that.scrollTop=_height;
-				that.showinfo=true;
-			},500)
+			// var that=this;
+			// setTimeout(()=>{
+			// 	// debugger;
+			// 	var _height=$("#all-system-info").height();
+			// 	that.scrollTop=_height;
+			// 	that.showinfo=true;
+			// },500)
 		},
 		computed: {
 			...mapGetters({
@@ -50,6 +51,7 @@
 		},
 		methods: {
 			async getinfos() {
+				// 按照最新消息排序,最新的显示在前面
 				var userInfo = uni.getStorageSync("userInfo");
 				var res = await db.collection('jz-custom-systeminfo').where({
 						user_id: userInfo._id,
